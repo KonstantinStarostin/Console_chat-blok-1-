8e20f59b37103add6b693473b5a8e990f86e61c7
KonstantinStarostin <101243903+KonstantinStarostin@users.noreply.github.com>
2022-07-14 21:57:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,26 +2,20 @@
  
  Chat with one public (for all users) and many private (for two users) rooms
  Allow register new unique users & excahge messages with all users or only one
-
  Users have unique ID number, unique name, password (8 simbols, literal, numbers, spec simbols)
-
 */
 
 /* general logic 
-
 main -> user_reg
      -> user_login
      -> user_change
      -> room_select
      -> chat_info
-
 room_select -> room_search_user
             -> room_search_all
-
 room_search_user/
 room_search_all / -> /room_print
                      /room_chat
-
 */
 
 #include <iostream>
@@ -32,18 +26,18 @@ room_search_all / -> /room_print
 #include "Room.h"
 
 // global vars
-Chat::User    *user    = nullptr;         // current user
-Chat::Message *message = nullptr;         // current message
-Chat::Room    *room    = nullptr;         // current room
+chat::User    *user    = nullptr;         // current user
+chat::Message *message = nullptr;         // current message
+chat::Room    *room    = nullptr;         // current room
 
                                           // all users, messages and chat rooms
-std::vector<Chat::User>    all_users;     //
-std::vector<Chat::Message> all_messages;  //
-std::vector<Chat::Room>    all_rooms;     //
+std::vector<chat::User>    all_users;     //
+std::vector<chat::Message> all_messages;  //
+std::vector<chat::Room>    all_rooms;     //
 
                                           
 
-Interface::Command cmd = Interface::Command::NO_COMMAND;    // command to work with chat
+interface::Command cmd = interface::Command::NO_COMMAND;    // command to work with chat
 
 /////////////////////////////////////////////  declaration block  /////////////////////////////////////////////////
 
@@ -77,35 +71,35 @@ int main(){
     
     while (true) {
         std::cout << "                      Welcome to chat\n\n";
-        std::cout << "Input >>reg  >>log  >>chg  >>room  >>info  >>quit command:  ";
-        std::string str = "";
+        std::cout << "Input >>reg  >>log  >>chg  >>room  >>info  >>quit command: ";
+        std::string str;
         std::cin >> str;
-        cmd = Interface::get_command(str);
+        cmd = interface::get_command(str);
         switch (cmd) {
-        case Interface::Command::REG:
+        case interface::Command::REG:
             if (user_reg())
                 std::cout << "User " << user->get_name() << " register in chat\n";
             break;
-        case Interface::Command::LOGIN:
+        case interface::Command::LOGIN:
             if (user_login())
                 std::cout << "User " << user->get_name() << " login in chat\n";
             break;
-        case Interface::Command::CHANGE:
+        case interface::Command::CHANGE:
             if (!user)
                 std::cout << "No user are login in chat!\n";
             else
                 user_change();
             break;
-        case Interface::Command::ROOM:
+        case interface::Command::ROOM:
             if (!user)
                 std::cout << "No user are login in chat!\n";
             else
                 room_select();
             break;
-        case Interface::Command::INFO:
+        case interface::Command::INFO:
             chat_info();
             break;
-        case Interface::Command::QUIT:
+        case interface::Command::QUIT:
             return 0;
         default:
             break;
@@ -120,44 +114,44 @@ void init_chat() {
     
 
     // Init some users
-    Chat::User cur_user;
+    chat::User cur_user;
    
-    cur_user = Chat::User("Ivan", "!234Qwer");  // ID = 1
+    cur_user = chat::User("Ivan", "!234Qwer");  // ID = 1
     all_users.push_back(cur_user);
-    cur_user = Chat::User("Bob", "123$qweR");   // ID = 2
+    cur_user = chat::User("Bob", "123$qweR");   // ID = 2
     all_users.push_back(cur_user);
-    cur_user = Chat::User("Inna", "1@34qWer");  // ID = 3
+    cur_user = chat::User("Inna", "1@34qWer");  // ID = 3
     all_users.push_back(cur_user);
-    cur_user = Chat::User("Sonya", "12#4qwEr"); // ID = 4
+    cur_user = chat::User("Sonya", "12#4qwEr"); // ID = 4
     all_users.push_back(cur_user);
     
     // Init chat for all users
-    Chat::Room cur_room;  
+    chat::Room cur_room;  
 
     // Fill with some messages
-    Chat::Message cur_message;
-    cur_message = Chat::Message(1, "Hello!");
+    chat::Message cur_message;
+    cur_message = chat::Message(1, "Hello!");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(1, "This is our work chat!");
+    cur_message = chat::Message(1, "This is our work chat!");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(3, "Order: Green street, 17");
+    cur_message = chat::Message(3, "Order: Green street, 17");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(3, "Boiler repairing");
+    cur_message = chat::Message(3, "Boiler repairing");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(2, "Where are instruments?");
+    cur_message = chat::Message(2, "Where are instruments?");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(1, "In truck!");
+    cur_message = chat::Message(1, "In truck!");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(2, "OK, I go");
+    cur_message = chat::Message(2, "OK, I go");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
-    cur_message = Chat::Message(4, "Stop, get offer list!");
+    cur_message = chat::Message(4, "Stop, get offer list!");
     all_messages.push_back(cur_message);
     cur_room.add_message_ID(cur_message.get_ID());
     
@@ -167,9 +161,9 @@ void init_chat() {
 }
 
 bool user_reg() {
-    Chat::User cur_user;
+    chat::User cur_user;
 
-    cur_user.set_name(&all_users);
+    cur_user.set_name(all_users);
     cur_user.set_pass();
     cur_user.reg();
     all_users.push_back(cur_user);
@@ -179,13 +173,13 @@ bool user_reg() {
 }
 
 bool user_login() {
-    Chat::User *cur_user = nullptr;
-    std::string str = "";
+    chat::User *cur_user = nullptr;
+    std::string str;
     do {
-        Chat::user_name_req();
+        chat::user_name_req();
         std::cout << "Input user name:  ";
         std::cin >> str;
-    } while (!Chat::user_name_valid(str));
+    } while (!chat::user_name_valid(str));
 
     for (int i = 0; i < all_users.size(); i++) {
         if (str == all_users[i].get_name()) {
@@ -217,18 +211,18 @@ void user_change() {
     while (true) {
         std::cout << "Input >>name  >>pass  >>quit command:  ";
 
-        std::string str = "";
+        std::string str;
         std::cin >> str;
 
-        cmd = Interface::get_command(str);
+        cmd = interface::get_command(str);
         switch (cmd) {
-        case Interface::Command::NAME:
-            user->set_name(&all_users);
+        case interface::Command::NAME:
+            user->set_name(all_users);
             break;
-        case Interface::Command::PASSWORD:
+        case interface::Command::PASSWORD:
             user->set_pass();
             break;
-        case Interface::Command::QUIT:
+        case interface::Command::QUIT:
             return;
         default:
             break;
@@ -238,9 +232,9 @@ void user_change() {
 
 int find_user_ID(std::string str) {
     int num = -1;
-    for (int i = 0; i < all_users.size(); i++) {
-        if (all_users[i].get_name() == str) {
-            num = all_users[i].get_ID();
+    for (const auto& all_user : all_users) {
+        if (all_user.get_name() == str) {
+            num = all_user.get_ID();
             break;
         }
     }
@@ -248,10 +242,10 @@ int find_user_ID(std::string str) {
 }
 
 std::string find_user_name(int ID) {
-    std::string name = "";
-    for (int i = 0; i < all_users.size(); i++) {
-        if (all_users[i].get_ID() == ID) {
-            name = all_users[i].get_name();
+    std::string name;
+    for (const auto& all_user : all_users) {
+        if (all_user.get_ID() == ID) {
+            name = all_user.get_name();
             break;
         }
     }
@@ -272,22 +266,22 @@ void room_select() {
 
         std::cout << "Input >>user  >>all  >>quit command:  ";
 
-        std::string str = "";
+        std::string str;
         std::cin >> str;
 
-        cmd = Interface::get_command(str);
+        cmd = interface::get_command(str);
         switch (cmd) {
-        case Interface::Command::USER:
+        case interface::Command::USER:
             if (room_search_user())
                 room_chat();
             room = nullptr;
             break;
-        case Interface::Command::ALL:
+        case interface::Command::ALL:
             if (room_search_all())
                 room_chat();
             room = nullptr;
             break;
-        case Interface::Command::QUIT:
+        case interface::Command::QUIT:
             room = nullptr;
             return;
         default:
@@ -300,12 +294,12 @@ void room_select() {
 
 bool room_search_user() {
  
-    std::string str = "";
+    std::string str;
     do {
-        Chat::user_name_req();
+        chat::user_name_req();
         std::cout << "Input user name:  ";
         std::cin >> str;
-    } while (!Chat::user_name_valid(str));
+    } while (!chat::user_name_valid(str));
     
     int user_ID = find_user_ID(str);
 
@@ -333,7 +327,7 @@ bool room_search_user() {
         return true;
 
     // create new private room
-    Chat::Room new_priv_room(user->get_ID(), user_ID, true);
+    chat::Room new_priv_room(user->get_ID(), user_ID, true);
     all_rooms.push_back(new_priv_room);
     room = &(all_rooms[all_rooms.size() - 1]);
     return true;
@@ -360,15 +354,15 @@ void room_chat() {
 
     while (true) {
              
-        std::string str = "";
+        std::string str;
         std::getline(std::cin, str);
         if (str != "") {
-            cmd = Interface::get_command(str);
+            cmd = interface::get_command(str);
             switch (cmd) {
-            case Interface::Command::QUIT:
+            case interface::Command::QUIT:
                 return;
             default:
-                Chat::Message msg(user->get_ID(), str);
+                chat::Message msg(user->get_ID(), str);
                 all_messages.push_back(msg);
                 room->add_message_ID(msg.get_ID());
                 break;
@@ -379,8 +373,8 @@ void room_chat() {
 }
 
 void room_print() {
-    std::string name = "";
-    std::string text = "";
+    std::string name;
+    std::string text;
     std::vector<int> cur_messages_ID = room->get_message_ID();
     std::cout << std::endl;
     for (int i = 0; i < cur_messages_ID.size(); i++) {
@@ -417,3 +411,4 @@ void chat_info() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
