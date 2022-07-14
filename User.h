f81a9730a5f77852bf4a30d0f89a237cbeabbfb7
KonstantinStarostin <101243903+KonstantinStarostin@users.noreply.github.com>
2022-07-14 21:56:46 +0300

    Update User.h

--- User.h
+++ User.h
@@ -1,33 +1,38 @@
 #pragma once
+
 #include <string>
 #include <vector>
 
 // Contain information for one unique (by ID & name) user
 
-namespace Chat {
+namespace chat {
 
 	class User {
 	public:
-		User() : ID(-1), name(""), pass(""), is_reg(false) {};
+		
+		User() : _ID(-1), _name(""), _pass(""), _is_reg(false)
+		{
+			
+		}
 
-		User(std::string _name, std::string _pass) : ID(max_ID + 1), name(_name), pass(_pass)
+		User(const std::string& name, const std::string& pass) : _ID(max_ID + 1), _name(name), _pass(pass)
 		{
 			max_ID++;
-			is_reg = true;
+			_is_reg = true;
 		}
 		
 		bool reg();
 
-		bool set_name(const std::vector<Chat::User>* all_users);
-		bool set_name(std::string _name, const std::vector<Chat::User>* all_users);
+		bool set_name(const std::vector<chat::User>& all_users);
+		bool set_name(const std::string& name, const std::vector<chat::User>& all_users);
 
 		bool set_pass();
-		bool set_pass(std::string _pass);
+		bool set_pass(const std::string& pass);
 
-		int         get_ID()   const { return ID; }
-		std::string get_name() const { return name; }
+		int         get_ID()   const { return _ID; }
+		std::string get_name() const { return _name; }
 		
-		bool check_pass(std::string _pass) { return (_pass == pass ? true : false); }
+		bool check_pass(const std::string& pass) const { return (pass == _pass ? true : false); }
 
 		// block for testing
 		/*void print_info();
@@ -35,27 +40,28 @@ namespace Chat {
 		void print_name();
 		void print_pass();*/
 		//
-
+		
 		User operator=(User user);
 
 	private:
 
-		int         ID;
-		std::string name;
-		std::string pass;
-		bool        is_reg;
+		int         _ID; 
+		std::string _name;
+		std::string _pass;
+		bool        _is_reg;
 
 		// counter for all users in chat
 		static int  max_ID;
 	};
 
 	// auxiliary function to work with name & password
-	bool uniq_user_name(std::string str, const std::vector<Chat::User>* all_users);
-	bool user_name_valid(std::string str);
+	bool uniq_user_name(const std::string& str, const std::vector<chat::User>& all_users);
+	bool user_name_valid(const std::string& str);
 	void user_name_req();
 
-	bool user_pass_valid(std::string str);
+	bool user_pass_valid(const std::string& str);
 	void user_pass_req();
 
 }
+
 
