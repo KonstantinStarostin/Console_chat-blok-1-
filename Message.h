73b446605c1c3639f957852565bc95e608be327c
KonstantinStarostin <101243903+KonstantinStarostin@users.noreply.github.com>
2022-07-14 21:55:38 +0300

    Update Message.h

--- Message.h
+++ Message.h
@@ -4,28 +4,28 @@
 
 // Contain one message (text) and information (user ID) about user which send this
 
-namespace Chat {
+namespace chat {
 
 	class Message {
 	public:
-		Message() : ID(-1), user_ID(-1), text("") {};
+		Message() : _ID(-1), _user_ID(-1), _text("") {};
 
-		Message(int _user_ID, std::string _text) : ID(max_ID + 1), user_ID(_user_ID), text(_text)
+		Message(int user_ID, const std::string& text) : _ID(max_ID + 1), _user_ID(user_ID), _text(text)
 		{
 			max_ID++;
 		};
 
-		int         get_ID()       { return ID; }
-		int         get_user_ID()  { return user_ID; }
-		std::string get_text()     { return text; }
+		int         get_ID()      const { return _ID; }
+		int         get_user_ID() const { return _user_ID; }
+		std::string get_text()    const { return _text; }
 
 		Message operator=(Message msg);
 
 	private:
 
-		int         ID;
-		int         user_ID;
-		std::string text;
+		int         _ID;
+		int         _user_ID;
+		std::string _text;
 
 		// counter for all messages in chat
 		static int  max_ID;
